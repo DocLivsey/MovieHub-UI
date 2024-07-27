a4bea3d059dc0491fe45421d151b31245c70d072
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-07-27 15:24:44 +0300

    create default spring project via spring initializr

---
+++ src/main/java/dclvs/moviehubui/MovieHubUiApplication.java
@@ -0,0 +1,13 @@
+package dclvs.moviehubui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieHubUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieHubUiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dclvs/moviehubui/MovieHubUiApplicationTests.java
@@ -0,0 +1,13 @@
+package dclvs.moviehubui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieHubUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
