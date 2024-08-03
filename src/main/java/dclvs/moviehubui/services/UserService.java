638c289f59bc9ac5b571b6883b094e3b1f6b6f90
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-08-03 23:14:49 +0300

    create user`s service

---
+++ src/main/java/dclvs/moviehubui/services/UserService.java
@@ -0,0 +1,15 @@
+package dclvs.moviehubui.services;
+
+import dclvs.moviehubui.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+}
