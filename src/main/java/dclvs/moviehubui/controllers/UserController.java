4fdb4df583c77e7c50caec05b8fe43a82c2ff111
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-08-04 21:43:48 +0300

    create users REST controller

---
+++ src/main/java/dclvs/moviehubui/controllers/UserController.java
@@ -0,0 +1,13 @@
+package dclvs.moviehubui.controllers;
+
+import dclvs.moviehubui.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class UserController {
+
+    private UserRepository userRepository;
+
+}
