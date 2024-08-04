68d8dfcc10cda10d2e788d0279b17a664af2009a
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-08-04 21:44:37 +0300

    inherited JpaRepository class

--- src/main/java/dclvs/moviehubui/repositories/UserRepository.java
+++ src/main/java/dclvs/moviehubui/repositories/UserRepository.java
@@ -1,13 +1,14 @@
 package dclvs.moviehubui.repositories;
 
 import dclvs.moviehubui.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findUserById(int id);
 
