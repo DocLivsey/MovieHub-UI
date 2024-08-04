c01a3689a2a0e9b5bd9194953434343aa9f4f1b5
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-08-04 22:55:10 +0300

    solve userRepo bean creating problem by adding query in find all method

--- src/main/java/dclvs/moviehubui/repositories/UserRepository.java
+++ src/main/java/dclvs/moviehubui/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package dclvs.moviehubui.repositories;
 
 import dclvs.moviehubui.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,6 +13,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findUserById(Long id);
 
+    @Query("select u from User u")
     List<User> findAllUsers();
 
 }
