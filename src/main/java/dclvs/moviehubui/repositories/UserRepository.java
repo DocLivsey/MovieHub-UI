f00d49fe0ebc5a32a8c1d21eeeeca640820fac89
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-08-04 21:45:58 +0300

    changed method`s parameters (for 'id': int => long)

--- src/main/java/dclvs/moviehubui/repositories/UserRepository.java
+++ src/main/java/dclvs/moviehubui/repositories/UserRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findUserById(int id);
+    Optional<User> findUserById(Long id);
 
     List<User> findAllUsers();
 
