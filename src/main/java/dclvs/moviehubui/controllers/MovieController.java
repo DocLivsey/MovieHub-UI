d3a9de89c6e972e24f911961345f55a3f881a6cf
DocLivsey <doclivsey@github.com>
2025-02-06 00:53:04 +0300

    feat: changed from interface to implemented

--- src/main/java/dclvs/moviehubui/controllers/MovieController.java
+++ src/main/java/dclvs/moviehubui/controllers/MovieController.java
@@ -2,12 +2,14 @@ package dclvs.moviehubui.controllers;
 
 import dclvs.moviehubui.clients.MovieHubAPIClient;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class MovieController {
 
-    private final MovieHubAPIClient movieHubAPIClient;
+    private final MovieHubAPIClient movieHubAPIClientImpl;
 
 }
