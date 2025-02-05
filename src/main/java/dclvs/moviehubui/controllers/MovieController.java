c20352d30851ca6309651db30a29d611ad22b1f5
DocLivsey <doclivsey@github.com>
2025-02-06 00:43:06 +0300

    feat: completely init as @bean controller

--- src/main/java/dclvs/moviehubui/controllers/MovieController.java
+++ src/main/java/dclvs/moviehubui/controllers/MovieController.java
@@ -1,4 +1,9 @@
 package dclvs.moviehubui.controllers;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
 public class MovieController {
 }
