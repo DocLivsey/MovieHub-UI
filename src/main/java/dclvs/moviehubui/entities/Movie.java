c3a3ec73ffad88e08f6289124d746e03619d3a9a
DocLivsey <doclivsey@github.com>
2025-02-06 13:19:21 +0300

    feat: start to write db entity for movie objcts

--- src/main/java/dclvs/moviehubui/entities/Movie.java
+++ src/main/java/dclvs/moviehubui/entities/Movie.java
@@ -1,4 +1,38 @@
 package dclvs.moviehubui.entities;
 
+import dclvs.moviehubui.dto.components.ExternalId;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Movie {
+
+    @Id
+    @GeneratedValue()
+    private Long id;
+
+    @Column(unique = true)
+    private ExternalId externalId;
+
+    private String name;
+
+    @Column(nullable = false)
+    private String type;
+
+    @Column(nullable = false)
+    private Integer year;
+
+
+
+
+
 }
