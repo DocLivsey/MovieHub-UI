b71fea780e42a1f558010070052ecebf30a27291
DocLivsey <doclivsey@github.com>
2025-03-08 21:53:42 +0300

    feat: new method to handle http get request to take all users

--- src/main/java/dclvs/moviehubui/controllers/UserController.java
+++ src/main/java/dclvs/moviehubui/controllers/UserController.java
@@ -3,12 +3,21 @@ package dclvs.moviehubui.controllers;
 import dclvs.moviehubui.repositories.UserRepository;
 import dclvs.moviehubui.services.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping("/all/")
+    public ResponseEntity<?> listAllUsers() {
+        return userService.listAllUsers();
+    }
 
 }
