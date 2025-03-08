b7ca861bce7f53dd5c30be602ca8cf66f1904284
DocLivsey <doclivsey@github.com>
2025-03-08 21:45:43 +0300

    feat: service method to get user by id

--- src/main/java/dclvs/moviehubui/services/UserService.java
+++ src/main/java/dclvs/moviehubui/services/UserService.java
@@ -1,9 +1,13 @@
 package dclvs.moviehubui.services;
 
+import dclvs.moviehubui.entities.User;
 import dclvs.moviehubui.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 
 @Slf4j
 @Service
@@ -11,5 +15,20 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    private ResponseEntity<?> findUserBy(Long Id) {
+        if (Id == null)
+            return ResponseEntity
+                    .unprocessableEntity()
+                    .build();
+
+        User user = userRepository
+                .findById(Id)
+                .orElseThrow(
+                        () -> new HttpClientErrorException(HttpStatus.NOT_FOUND)
+                );
+
+        return ResponseEntity.ok(user);
+    }
 
 }
