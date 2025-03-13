67844962739a8478ea527275c25c261b28550255
DocLivsey <doclivsey@github.com>
2025-03-13 20:39:38 +0300

    feat: reimport Entity component

--- src/main/java/dclvs/moviehubui/entities/components/CurrencyValueEntity.java
+++ src/main/java/dclvs/moviehubui/entities/components/CurrencyValueEntity.java
@@ -1,11 +1,12 @@
 package dclvs.moviehubui.entities.components;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.experimental.Accessors;
+
+import javax.persistence.Entity;
 
 @Data
 @Entity
