6b1107241321247ff7022450e9b7111fe5e57172
DocLivsey <doclivsey@users.noreply.github.com>
2025-03-19 22:34:19 +0300

    feat: init new classes for movie's entities logic

---
+++ src/main/java/dclvs/moviehubui/services/MovieService.java
@@ -0,0 +1,11 @@
+package dclvs.moviehubui.services;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class MovieService {
+}
