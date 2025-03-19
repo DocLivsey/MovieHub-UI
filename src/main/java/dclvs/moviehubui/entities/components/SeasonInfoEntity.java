9aa69e36d90fada35b2e2ebf351f9e9ccb6c6d86
DocLivsey <doclivsey@github.com>
2025-03-19 19:06:54 +0300

    fix: missing id field

--- src/main/java/dclvs/moviehubui/entities/components/SeasonInfoEntity.java
+++ src/main/java/dclvs/moviehubui/entities/components/SeasonInfoEntity.java
@@ -1,5 +1,8 @@
 package dclvs.moviehubui.entities.components;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.experimental.Accessors;
@@ -11,6 +14,10 @@ import javax.persistence.Entity;
 @Accessors(chain = true)
 @Table(name = "seasons")
 public class SeasonInfoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private Integer number;
 
