73586f5283904985ff6489b33b1114bb9cc18447
DocLivsey <doclivsey@github.com>
2025-03-21 23:39:06 +0300

    feat: appended repository with getting user's movie by id

--- src/main/java/dclvs/moviehubui/repositories/MovieRepository.java
+++ src/main/java/dclvs/moviehubui/repositories/MovieRepository.java
@@ -1,9 +1,18 @@
 package dclvs.moviehubui.repositories;
 
 import dclvs.moviehubui.entities.Movie;
+import dclvs.moviehubui.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    @Query("select m from Movie m where m.id = :id and m.user = :user")
+    Optional<Movie> findUsersMovieById(@Param("id") Long id, @Param("user") User user);
+
 }
