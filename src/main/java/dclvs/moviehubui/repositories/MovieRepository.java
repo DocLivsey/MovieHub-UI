023c818d800b8bc63a721828fd99e534d02f1798
DocLivsey <doclivsey@github.com>
2025-03-22 12:15:30 +0300

    feat: getting all user's movies

--- src/main/java/dclvs/moviehubui/repositories/MovieRepository.java
+++ src/main/java/dclvs/moviehubui/repositories/MovieRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,5 +16,8 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
     @Query("select m from Movie m where m.id = :id and m.user = :user")
     Optional<Movie> findUserMovieById(@Param("id") Long id,
                                        @Param("user") User user);
+
+    @Query("select m from Movie m where m.user = :user")
+    Optional<List<Movie>> findAllUserMovies(@Param("user") User user);
 
 }
