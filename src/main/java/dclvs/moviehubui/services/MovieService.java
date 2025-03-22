04bc34e955244e39dc78f2858df39e25410c063a
DocLivsey <doclivsey@github.com>
2025-03-22 12:22:21 +0300

    feat: init field

--- src/main/java/dclvs/moviehubui/services/MovieService.java
+++ src/main/java/dclvs/moviehubui/services/MovieService.java
@@ -1,11 +1,18 @@
 package dclvs.moviehubui.services;
 
+import dclvs.moviehubui.entities.Movie;
+import dclvs.moviehubui.repositories.MovieRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
 @AllArgsConstructor
 public class MovieService {
+
+    private final MovieRepository movieRepository;
+
 }
