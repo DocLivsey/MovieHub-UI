e0e0bb5849a16aea340c0f71858b01c71fc7895a
DocLivsey <doclivsey@github.com>
2025-03-22 20:12:38 +0300

    fix: remove redundant insertion method

--- src/main/java/dclvs/moviehubui/repositories/MovieRepository.java
+++ src/main/java/dclvs/moviehubui/repositories/MovieRepository.java
@@ -2,7 +2,9 @@ package dclvs.moviehubui.repositories;
 
 import dclvs.moviehubui.entities.Movie;
 import dclvs.moviehubui.entities.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -19,8 +21,6 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
 
     @Query("select m from Movie m where m.user = :user")
     Optional<List<Movie>> findAllUserMovies(@Param("user") User user);
-
-    void addUserMovie(Movie movie, User user);
 
     void deleteUserMovie(Long id, User user);
 
