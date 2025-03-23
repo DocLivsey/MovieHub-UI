36721135a08ad1286e624eae62a4f312661f09db
DocLivsey <doclivsey@github.com>
2025-03-23 11:46:39 +0300

    feat: new methods to get all movies from db

--- src/main/java/dclvs/moviehubui/services/MovieService.java
+++ src/main/java/dclvs/moviehubui/services/MovieService.java
@@ -1,10 +1,14 @@
 package dclvs.moviehubui.services;
 
 import dclvs.moviehubui.clients.MovieHubAPIClient;
+import dclvs.moviehubui.entities.Movie;
 import dclvs.moviehubui.repositories.MovieRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -14,5 +18,12 @@ public class MovieService {
     private final MovieRepository movieRepository;
 
     private MovieHubAPIClient movieHubAPIClientImpl;
+
+    public ResponseEntity<?> allMovies() {
+        List<Movie> movies = movieRepository.findAll();
+        return movies.isEmpty()
+                ? ResponseEntity.noContent().build()
+                : ResponseEntity.ok(movies);
+    }
 
 }
